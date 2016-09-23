03338b08e78f26856b55e01729821722ee049c3f
WU Yue <hermione_54@163.com>
2016-09-23 13:57:40 -0400

    first commit

---
+++ GraphCalculator/FunctionPlottingView.swift
@@ -0,0 +1,35 @@
+//
+//  FunctionPlottingView.swift
+//  GraphCalculator
+//
+//  Created by Yanpu Wu on 9/22/16.
+//  Copyright © 2016 Yanpu Wu. All rights reserved.
+//
+
+import UIKit
+
+class FunctionPlottingView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
